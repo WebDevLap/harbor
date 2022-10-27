ed8c90fe2b3c68195c93d955f2bdc22cd456490b
WebDevLap <114002073+WebDevLap@users.noreply.github.com>
2022-10-27 15:50:34 +0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,11 +8,16 @@ const body = document.body
 
 document.addEventListener('click', function(event){
     const targetBurger = event.target.closest('.menu__burger')
+    const headerMenuWrapper = event.target.closest('.header__menu-wrapper')
 
+    if(!headerMenuWrapper){
+        menu.classList.remove('active')
+        burger.classList.remove('active')
+        body.classList.remove('lock')
+        return
+    }
     if(!targetBurger) return;
-
     menu.classList.toggle('active')
     burger.classList.toggle('active')
     body.classList.toggle('lock')
-    
 })
